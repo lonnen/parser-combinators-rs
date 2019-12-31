2d419660be41c588a1daf654cb365a275f48a6f6
lonnen <chris.lonnen@gmail.com>
2019-12-30 19:05:59 -0800

    our first parser

--- src/lib.rs
+++ src/lib.rs
@@ -42,3 +42,84 @@ struct Element {
     attributes: Vec<(String, String)>,
     children: Vec<Element>,
 }
+
+/// Parsers transform a stream of data by giving it structure.
+/// 
+/// Any given parser is going to be limited in it's capability. It may only
+/// parse some of the input, and return unparsed input alongside parsed output.
+/// If it can't parse any of it, we want to return an error.
+///
+/// Something like this:
+/// ```
+/// Fn(Input) -> Result<(Input, Output), Error>
+/// ```
+///
+/// In implementation we need to be more precise for the compiler, so we'll
+/// add some types.
+
+/// Fn(&str) -> Result<(&str, Element), &str>
+
+/// For the moment we're going to return problematic string slices instead of
+/// real errors.
+
+/// String slices are efficient pointers to a piece of a string. We can consume
+/// the input in sub-slices and return the remainder.
+
+/// Using a string slice gives us a lot of useful methods in Rust, though as
+/// Bodil points out we could have used &[u8] instead here. This corresponds to
+/// single ASCII characters and allows direct indexing of the slice. Unicode is
+/// complicated, though, and we're going to leverage the standard lib to manage
+/// it for us. Dealing with Unicode here would be something of a distraction
+/// for this excercise but important to any industrial parser.
+
+
+/// Let's begin with a parser examines the first character in the string and
+/// extracts it, iff it is `a`
+
+/// In this example we're not even using the Element struct since this parser
+/// is ridiculously simple. It can only succeed in one case, so we return the
+/// unit type `()` since the letter 'a' can be assumed. This is probably more
+/// complicated than it's worth since it required a whole paragraph to explain
+/// rather than just returning 'a', but I'm sticking to the original text for
+/// now.
+
+/// Anyhow, the intent was to let us focus on manipulating the input streams,
+/// so let's just look at that. The standard library methods that we
+/// anticipated needing above are the bread-and-butter of this parser.
+/// We're letting it handle splitting the slice into an interator of chars and
+/// so we can simply pull the 'next' item off of it.
+
+/// Since the slice could be empty, what we get back is an `Option<char>`
+/// which will contain the next `char` or a `None` if it's empty.
+
+/// A `char` isn't as simple as it seems, though, because Unicode abstracts
+/// over ... a lot. We could address this by messing directly with bytes as
+/// discussed above, but instead let's do what almost every workaday programmer
+/// does and pretend a Unicode `char` matches our ASCII-founded intuitions and
+/// we'll ignore graphemes, clusters, scalars, and all the intermediary
+/// abstractions between bytes and semantic value. Again, it's enough to know
+/// that if we start accepting input written by other people we'll have to deal
+/// with that stuff sooner or later. For this demo we can call it out of scope.
+
+/// Pattern matching will do a lot of heavy lifting in our parser combinators.
+/// It's no coincidence that Haskell also has advanced pattern matching and
+/// one of it's greatest industry use cases is parsing. All Rust parsing libs
+/// liberally use this language feature.
+
+/// `Some('a')` is our specific result and matching against it directly keeps
+/// our parser concise. If the character looks good, we'll return the rest of
+/// the input, sans `a`. If there's no char or the character is not the
+/// precise one we expect then we'll return an error. Not a useful error, so
+/// that's something to improve.
+
+/// This function isn't directly useful for our task, but it looks a lot like
+/// the start of an element parser. We'll need to recognize several single
+/// chars. It seems like we'll need this in a few places, which usually means
+/// it's time for an abstraction.
+
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
